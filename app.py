b039666121eca311826f013d829c0384817acda7
michaelslyusar <michael.slyusar@gmail.com>
2019-10-01 17:15:09 +0200

    Internal server error - logging added

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask, request
 from flask_restful import Api
 from resources.Lead import get_lead_by_stat, create_lead
-import sys
-import logging
 
 
 
 
 app = Flask(__name__)
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://oAJmLqZI4I:otYDdVSoi9@remotemysql.com:3306/oAJmLqZI4I'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
