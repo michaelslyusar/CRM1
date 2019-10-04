36e3d9d27406c88165e25c9dab0574cda73d722d
michaelslyusar <michael.slyusar@gmail.com>
2019-10-04 11:27:29 +0200

    DB fix

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from resources.Lead import get_lead_by_stat, create_lead
 import sys
 import logging
 
-#os.environ.get('HEROKU_POSTGRESQL_MAROON_URL', 'mysql://oAJmLqZI4I:otYDdVSoi9@remotemysql.com:3306/oAJmLqZI4I')
-#postgres://mofoqkhizyxivu:8c1bbd9033833f14e037c6a44ef9f6228bd2c409dad0e517e2cc3f35ad5775f4@ec2-54-247-188-247.eu-west-1.compute.amazonaws.com:5432/dem6ieah3nsp8l
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
@@ -15,13 +13,9 @@ app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:Aa123456@database-4.crywuregr34q.eu-central-1.rds.amazonaws.com:3306/crm159'
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 api.add_resource(create_lead, '/create_lead')
 api.add_resource(get_lead_by_stat, '/LeadList_dep')
